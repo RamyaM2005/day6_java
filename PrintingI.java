bf02e4a6dca21522db41d733100c1678d41dad39
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-07 10:23:18 +0530

    Add files via upload

---
+++ PrintingI.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class PrintingI
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if((n&1)!=1)
+                {
+                    n--;
+                }
+            if(i==n-1 || i==0 || j==(n/2))
+            {
+                System.out.print("*");
+
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    sc.close();
+    }
+}
